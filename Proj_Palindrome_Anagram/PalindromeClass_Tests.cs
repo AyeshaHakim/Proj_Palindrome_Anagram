572d5f29de19bde93b46b7fc38f18b730c34507a
Dr. Ayesha Hakim <a.hakim@auckland.ac.nz>
2016-12-14 16:55:37 +1300

    Adding Unit Test Project
    Add unit test project, write functions to test
    whether the string is just palindrome
    whether the string is just anagram
    whether the string is palindrome or anagram
    whether the string is palindrome and anagram

---
+++ Proj_Palindrome_Anagram/PalindromeClass_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Palindrome_Anagram
+{
+	public class PalindromeClass_Tests
+	{
+		//A test for is_palindrome function
+		void is_palindrome_true_test()
+		{
+			PalindromeClass cls_palindrome = new PalindromeClass(); 
+			String s = "anna";
+			bool actual = cls_palindrome.is_palindrome(s);
+			const bool expected = true;
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ Proj_UnitTestPalindrome/Test_isPalindrome.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+namespace Palindrome_Anagram
+{
+	[TestFixture()]
+	public class Test_isPalindrome
+	{
+		[TestCase("anna", Result=true)]
+		[TestCase("aaaaaaaa", Result = true)]
+		[TestCase("aaaaaaaab", Result = false)]
+		[TestCase("caaaaaaaab", Result = false)]
+		[TestCase("donotbobtonod", Result = true)]
+		[TestCase("owefhijpfwai", Result = false)]
+		[TestCase("igdedgide", Result = false)]
+		public bool is_PalindromeTest(String s)
+		{
+			PalindromeClass cls_palindrome = new PalindromeClass();
+			return cls_palindrome.is_palindrome(s);
+		}
+
+		[TestCase("anna", Result = true)]
+		[TestCase("aaaaaaaa", Result = true)]
+		[TestCase("aaaaaaaab", Result = true)]
+		[TestCase("caaaaaaaab", Result = false)]
+		[TestCase("donotbobtonod", Result = true)]
+		[TestCase("owefhijpfwai", Result = false)]
+		[TestCase("igdedgide", Result = true)]
+		public bool is_AnagramTest(String s)
+		{
+			PalindromeClass cls_palindrome = new PalindromeClass();
+			return cls_palindrome.is_anagram(s);
+		}
+
+		[TestCase("anna", Result = true)]
+		[TestCase("aaaaaaaa", Result = true)]
+		[TestCase("aaaaaaaab", Result = true)]
+		[TestCase("caaaaaaaab", Result = false)]
+		[TestCase("donotbobtonod", Result = true)]
+		[TestCase("owefhijpfwai", Result = false)]
+		[TestCase("igdedgide", Result = true)]
+		public bool is_Palindrom_or_AnagramTest(String s)
+		{
+			PalindromeClass cls_palindrome = new PalindromeClass();
+			return cls_palindrome.is_palindrome(s) || cls_palindrome.is_anagram(s);
+		}
+
+		[TestCase("anna", Result = true)]
+		[TestCase("aaaaaaaa", Result = true)]
+		[TestCase("aaaaaaaab", Result = false)]
+		[TestCase("caaaaaaaab", Result = false)]
+		[TestCase("donotbobtonod", Result = true)]
+		[TestCase("owefhijpfwai", Result = false)]
+		[TestCase("igdedgide", Result = false)]
+		public bool is_Palindrom_and_AnagramTest(String s)
+		{
+			PalindromeClass cls_palindrome = new PalindromeClass();
+			return cls_palindrome.is_palindrome(s) && cls_palindrome.is_anagram(s);
+		}
+	}
+}
