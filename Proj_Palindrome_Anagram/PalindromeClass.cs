f4584fa302bb21ac84def3e74f34e93939b8ac3a
Dr. Ayesha Hakim <a.hakim@auckland.ac.nz>
2016-12-14 15:12:05 +1300

    LIC Coding Test by Ayesha Hakim

---
+++ Proj_Palindrome_Anagram/PalindromeClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+namespace Palindrome_Anagram
+{
+	public class PalindromeClass
+	{
+		// Check whether the entered string is valid
+		public bool is_invalidString(String str)
+		{
+			try
+			{
+				Regex r = new Regex("^[a-z]*$");
+				if (str.Length == 0)
+					return false;
+				else
+				{
+					if (r.IsMatch(str))
+						return true;
+					else
+						return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Something unexpected happened: {0}", ex.GetType());
+				return false;
+			}
+		}
+		// Check whether a string is a palindrome
+		public bool is_palindrome(String s)
+		{
+			try
+			{
+				String revs = "";
+				var length = s.Length;
+
+				for (int i = length - 1; i >= 0; i--) //String Reverse
+				{
+					revs += s[i].ToString();
+				}
+				if (revs == s)
+					return true;
+				else
+					return false;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Something unexpected happened: {0}", ex.GetType());
+				return false;
+			}
+		}
+		// Check whether a string is an anagram of a palindrome
+		public bool is_anagram(String s)
+		{
+			try
+			{
+				var length = s.Length;
+				var dic = new Dictionary<char, int>();
+				for (int i = 0; i < length; i++)
+				{
+					if (dic.ContainsKey(s[i]))
+					{
+						dic[s[i]]++;
+						continue;
+					}
+
+					dic.Add(s[i], 1);
+				}
+				int odd = 0;
+				foreach (var pv in dic)
+				{
+					if (pv.Value % 2 != 0)
+						odd++;
+				}
+				if (odd > 1)
+					return false;
+				else
+					return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Something unexpected happened: {0}", ex.GetType());
+				return false;
+			}
+		}
+	}
+}
---
+++ Proj_Palindrome_Anagram/Program.cs
@@ -0,0 +1,40 @@
+﻿// Program developed by Ayesha Hakim for LIC conding test (14-12-2016)
+// Git repo: https://github.com/AyeshaHakim/AYESHA_HAKIM_LIC_Coding
+using System;
+namespace Palindrome_Anagram
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string key="";
+			PalindromeClass cls_palindrome = new PalindromeClass();
+			while (key != "n")
+			{
+				Console.WriteLine("\n*** Enter a String ***\n");
+				Console.WriteLine("Instructions:");
+				Console.WriteLine("There should be no whitespace in the string");
+				Console.WriteLine("All characters in the string must be lower case");
+				Console.WriteLine("The string will only contain alpha characters a-z \n");
+				String s = Console.ReadLine();
+
+				while (!cls_palindrome.is_invalidString(s))
+				{
+					Console.WriteLine("Please enter a valid string \n");
+					s = Console.ReadLine();
+				}
+
+				if (cls_palindrome.is_palindrome(s))
+					Console.WriteLine("String is a Palindrome \n");
+				if (cls_palindrome.is_anagram(s))
+					Console.WriteLine("String is an Anagram of a Palindrome \n");
+				if(!cls_palindrome.is_palindrome(s) && !cls_palindrome.is_anagram(s))
+					Console.WriteLine("String is neither a Palindrome nor an Anagram of a Palindrome \n");
+				
+				Console.WriteLine("Do you want to continue? Press 'y' for YES and 'n' for NO");
+
+				key=Console.ReadKey().KeyChar.ToString();
+			}
+		}
+	}
+}
